9d44fa33d455d1db5975df900fcb63ac3e94dddd
Aryan <singharyan56338@gmail.com>
2026-09-08 13:16:08 +0530

    Initial MERN project

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>Hello Bhai! Server chal raha hai 🚀</h1>");
+});
+
+server.listen(5000, () => {
+  console.log("Server running at http://localhost:5000");
+});
